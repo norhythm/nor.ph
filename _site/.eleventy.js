49645598d6a8020e4487204756483aeb8f3c1d26
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2019-12-04 10:40:22 +0900

    fixed path

---
+++ _site/.eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+  return {
+    templateFormats: [
+      "njk",
+      "pug",
+      "css",
+      "png",
+      "jpg",
+      "js",
+      "ico",
+      "zip"
+    ]
+  };
+};
