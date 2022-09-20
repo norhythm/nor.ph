bf9329c772ca7d8c60fad0551f6dcce5048f65b3
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2022-09-20 19:58:10 +0900

    dir-to-jsonを利用してog:imageの作成に変更

--- screenshot.js
+++ screenshot.js
@@ -1,9 +1,18 @@
-const lists = require('./list.json');
 const puppeteer = require('puppeteer');
+const dirToJson = require('dir-to-json');
+let lists;
 
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+dirToJson("./works", { sortType: true })
+	.then(function (dirTree) {
+    lists = dirTree.children;
+	})
+	.catch(function (err) {
+		throw err;
+	});
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -16,18 +25,18 @@ function delay(ms) {
   const url = 'http://localhost:3000';
 
   for (list of lists) {
-    await page.goto(`${url}/${list}/`);
+    await page.goto(`${url}/works/${list.path}/`);
     await page.addStyleTag({url: `${url}/stylesheets/og.css`});
     await delay(1000);
     await page.screenshot({
-      path: `${list}/og.png`,
+      path: `works/${list.path}/og.png`,
       clip: {
         x: 0,
         y: 0,
         width: 1200,
         height: 630
     }});
-    console.log('Done:', list);
+    console.log('Done:', list.path);
   }
   await browser.close();
   console.log("Almost done!")
